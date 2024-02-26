30a3237e4654e4f9cd5f8bbb502f9c5fd6904fd9
Manish <manish.chilwal@clevertap.com>
2024-02-26 14:49:04 +0530

    Testing Features Application Native

--- app/src/main/java/com/example/testingfeatures/MainActivity.java
+++ app/src/main/java/com/example/testingfeatures/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.clevertap.android.pushtemplates.PushTemplateNotificationHandler;
 import com.clevertap.android.sdk.CleverTapAPI;
 import com.clevertap.android.sdk.displayunits.DisplayUnitListener;
 import com.clevertap.android.sdk.displayunits.model.CleverTapDisplayUnit;
+import com.clevertap.android.sdk.displayunits.model.CleverTapDisplayUnitContent;
 import com.clevertap.android.sdk.interfaces.NotificationHandler;
 
 import java.util.ArrayList;
@@ -52,5 +57,27 @@ public class MainActivity extends AppCompatActivity implements DisplayUnitListen
     @Override
     public void onDisplayUnitsLoaded(ArrayList<CleverTapDisplayUnit> units) {
         // you will get display units here
+        for (int i = 0; i < units.size(); i++) {
+            CleverTapDisplayUnit unit = units.get(i);
+            Log.v("Native", String.valueOf(unit));
+            prepareDisplayView(unit);
+        }
+    }
+
+    private void prepareDisplayView(CleverTapDisplayUnit unit) {
+
+        LinearLayout parentLayout = findViewById(R.id.linear);
+        for (CleverTapDisplayUnitContent contentItem : unit.getContents()) {
+            String media = contentItem.getMedia();
+            System.out.println("Media: " + media);
+            ImageView imageView = new ImageView(this);
+            Glide.with(this).load(media).into(imageView);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+            );
+            imageView.setLayoutParams(layoutParams);
+            parentLayout.addView(imageView);
+        }
     }
 }
